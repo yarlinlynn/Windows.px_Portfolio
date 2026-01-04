db6d7f713c37d7002ed829a2ead6dfc2b1a1aa9a
Yarlin <yarlinlynn@gmail.com>
2026-01-04 14:51:30 +0200

    Added custom css styles, changed website favicon and created & styled taskbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { useEffect, useRef, useState } from 'react'
 import { useScreen } from "./context/ScreenContext.jsx";
 
 // import img from './assets/images/windows_logo.png';
-import logo from './assets/images/logo.png';
-import desktopIocn from './assets/images/desktop_profile_icon.jpg';
+// import logo from './assets/images/logo.png';
+// import desktopIocn from './assets/images/desktop_profile_icon.jpg';
+// import startBtn from './assets/images/start_btn_normal.png';
 
 // import './App.css'
 
@@ -53,7 +54,7 @@ function App() {
         <p className='text-[20px] tracking-[1.5px] skew-x-[-10deg] fixed bottom-[1px] right-[5px]'>Portfolio</p>
      
       </section> */}
-      <section className='fixed w-screen h-screen bg-[#002d99] top-0 left-0 overflow-hidden text-[#ffffff]'>
+      {/* <section className='fixed w-screen h-screen bg-[#002d99] top-0 left-0 overflow-hidden text-[#ffffff]'>
         <div className="absolute top-[100px] bottom-[100px] left-[0px] right-[0px] bg-[#587cdb] 
         before:absolute before:top-0 before:left-0 before:right-0 before:w-full before:h-[3px] before:content-[''] before:bg-gradient-to-r before:from-transparent before:via-[#bad7f8] before:to-transparent 
         after:content-[''] after:absolute after:bottom-0 after:left-0 after:right-0 after:w-full after:h-[3px] after:bg-[linear-gradient(to_right,transparent,#f8953d,transparent,transparent)]">
@@ -77,7 +78,6 @@ function App() {
             <div className='flex w-px h-[24rem] bg-[linear-gradient(180deg,rgba(255,255,255,0)_0%,rgba(255,255,255,.48)_49.48%,rgba(255,255,255,0)_100%)]'></div>
             <div className="back-gradient relative group">
               <div>
-                {/* desktop icon goes here */}
                 <img src={desktopIocn} className='w-[70px] rounded-[5px] border-[3px] border-white transition-colors duration-300 opacity-[0.8] group-hover:border-[#fdbd32]' alt='Desktop Icon'/>
               </div>
               <p className='text-center group-hover:text-[#fdbd32] mt-[5px] tracking-[0.8px]'>Yarlin</p>
@@ -85,7 +85,24 @@ function App() {
           </div>
 
         </div>
-      </section>
+      </section> */}
+      <main className="bg-[url('./assets/images/desktop-background.jpg')] bg-no-repeat bg-cover bg-right w-full h-screen">
+      {/* main content */}
+        <section></section>
+        {/* taskbar */}
+        <section className='fixed bottom-[0px] left-[0px] w-full h-[30px] flex items-center flex-row bg-custom-gradient z-10'>
+          {/* start button */}
+          <div id="start-btn" className="bg-[url('./assets/icons/start_btn_normal.png')] bg-cover h-full w-[100px] hover:brightness-125 hover:[filter:brightness(1.1)] absolute bottom-[-0.3px] left-[-1px]"></div>
+          {/*  */}
+          <div className='flex justify-start grow h-full overflow-hidden pt-1 '></div>
+          <div className='flex justify-end items-center flex-row shrink-0 max-w-[200px] taskbar-right-bg h-full'>
+            <div className="bg-[url('./assets/icons/TourXP.png')] taskbar-icons "></div>
+            <div className="bg-[url('./assets/icons/SecurityError.png')] taskbar-icons "></div>
+            <div className="bg-[url('./assets/icons/Volume.png')] taskbar-icons "></div>
+            <span className='off-white text-[11px] px-1'>11:30 PM</span>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
