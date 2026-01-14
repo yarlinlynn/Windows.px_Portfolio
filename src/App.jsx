6348adf207bad3cfae0663c4395307d70babaf33
Yarlin <yarlinlynn@gmail.com>
2026-01-14 16:02:51 +0200

    Removed all images, components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { useScreen } from "./context/ScreenContext.jsx";
 
-import BootScreen from './components/Screens/BootScreen.jsx';
-import LoginScreen from './components/Screens/LoginScreen.jsx';
-import Desktop from './components/Screens/Desktop.jsx';
 
 function App() {
 
-  const { screen } = useScreen();
-
-  return (
-    <>
-
-    {screen === 'load' && <BootScreen/>}
-    {screen === 'login' && <LoginScreen/>}
-    {screen === 'desktop' && <Desktop/>}
-
-    </>
-  );
+ 
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { ScreenProvider } from './context/ScreenContext.jsx';
-
-import 'xp.css/dist/XP.css'
 import '../src/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ScreenProvider>
     <StrictMode>
       <App />
     </StrictMode>
-  </ScreenProvider>,
 )
